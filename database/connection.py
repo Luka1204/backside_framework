3b4a5b853a48c3232f1392b7ab4647817f5bcaf7
Reyes,luka <reyes.luka@moron.gob.ar>
2026-01-28 00:01:13 -0300

    Correcciones Routing, Global Middleware alpha@v0.0.1c

--- database/connection.py
+++ database/connection.py
@@ -1,8 +1,8 @@
 import sqlite3
 
 class Database:
-    def __init__(self, deb_name='database.db'):
-        sel.conn = sqlite3.connect(db_name)
+    def __init__(self, db_name='database.db'):
+        self.conn = sqlite3.connect(db_name)
         self.conn.row_factory = sqlite3.Row
     
 
